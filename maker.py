e00e636c68bbd0b9c461004c2510ace771cd54f2
Joseph Curtis <jccurtis@lbl.gov>
2021-11-02 10:14:01 -0700

    Add new maker python script to replace repo level make

---
+++ maker.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# Author: Joey Curtis @jccurtis
+import argparse
+import subprocess
+import shlex
+import platform
+from pathlib import Path
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="[%(asctime)s|%(levelname)-4s] %(message)s"
+)
+DOCKER_NAMESPACE = "lblanp"
+PARENT_DIR = Path(__file__).parent
+
+
+def build(dockerfile: Path, rebuild: bool = False) -> str:
+    # Handle tags / arch
+    extra_tags = [s.lstrip(".") for s in dockerfile.suffixes]
+    for t in extra_tags:
+        if t != t.lower():
+            raise ValueError(
+                f"Dockerfile suffix tags must all be lowercase: {dockerfile}"
+            )
+    image_arch = "x86_64"
+    if "l4t" in extra_tags:
+        logging.info(f"Found Linux 4 Tegra tag in {dockerfile}")
+        image_arch = "aarch64"
+    if "arm64v8" in extra_tags:
+        logging.info(f"Found ARM64v8 tag in {dockerfile}")
+        image_arch = "aarch64"
+    if image_arch != platform.machine():
+        logging.warning(
+            f"Cannot currently build across platforms (this={platform.machine()} "
+            f"vs requested={image_arch}) Skipping: {dockerfile}"
+        )
+        return
+    image_name = f"{DOCKER_NAMESPACE}/{dockerfile.parent.stem}"
+    if len(extra_tags):
+        image_name += "_" + "_".join(extra_tags)
+    # Prepare cmd
+    cmd = (
+        f"cd {dockerfile.parent} && "
+        f"docker build -t {image_name}:latest -f {dockerfile.name} "
+        f"{'--no-cache ' if rebuild else ''}"
+        "."
+    )
+    logging.info(f"Building: {cmd}")
+    subprocess.Popen(shlex.split(cmd), shell=True).wait()
+    return image_name
+
+
+def main(name: str, push: bool = False, rebuild: bool = False) -> None:
+    directory = Path(PARENT_DIR / name)
+    # Get dockerfiles
+    dfiles = list(directory.glob("Dockerfile*"))
+    if len(dfiles) == 0:
+        logging.error(f"No `Dockerfile*`s found: {directory}")
+        return
+    # Build images
+    for dfile in dfiles:
+        build(dfile, rebuild=rebuild)
+
+
+def cli():
+    # Get neighboring directories
+    neighbors = list(p.name for p in PARENT_DIR.iterdir() if p.is_dir())
+    # Command line handling
+    parser = argparse.ArgumentParser(
+        description="Utility for building/tagging/pushing docker images"
+    )
+    parser.add_argument(
+        "name",
+        type=str,
+        help="Docker image name",
+        choices=neighbors,
+    )
+    parser.add_argument(
+        "-p",
+        "--push",
+        action="store_true",
+        help="Run docker push",
+    )
+    parser.add_argument(
+        "-r",
+        "--rebuild",
+        action="store_true",
+        help="Rebuild with --no-cache option",
+    )
+    args = parser.parse_args()
+    main(**vars(args))
+
+
+if __name__ == "__main__":
+    cli()
